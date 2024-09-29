390732d5193c82324726aec747d512ebde987a54
bradybaughrobert-fs <rlbradybaugh@student.fullsail.edu>
2024-09-29 09:42:27 -0400

    updated App.jsx

--- Client/Client/src/App.jsx
+++ Client/Client/src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+// import Searchbar from "./components/Searchbar";
+// import API from "./API";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const handleSearch = async (event) => {
+    event.preventDefault();
+    console.log("Search button clicked!", event.target.search.value);
+    // const response = await API.searchCharacter(event.target.search.value);
+    //console.log("From Unsplash API", response);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Character Search</h1>
+      {/* <Searchbar onSearch={handleSearch} /> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
